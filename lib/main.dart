44603629937714ee6b09376f061c75c4168e4e77
Alex Popa <svn_alexandru@yahoo.com>
2020-11-18 15:42:35 +0200

    Form field validation, added button to initiate validation

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main()
 {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  MyApp({Key key}) : super(key: key);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
+  bool _isNumeric(String str) {
+    if(str == null) {
+      return false;
+    }
+    return double.tryParse(str) != null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +35,35 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Image.network('https://static.playtech.ro/wp-content/uploads/2020/08/bani-monede-digitale-banci.jpg'),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter the amount of money in EUR',
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Form(
+                key: _formKey,
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter the amount of money in EUR',
+                  ),
+                  // ignore: missing_return
+                  validator: (String value) {
+                    if(!_isNumeric(value)) {
+                        // ignore: unnecessary_parenthesis
+                        return('Please enter a number');
+                      }
+                    else {
+                      return null;
+                    }
+                  },
+                ),
               ),
+            ),
+            FlatButton(
+                onPressed: () {
+                  if(_formKey.currentState.validate()) {
+                  }
+                },
+                child: const Text('Convert!'),
+                color: Colors.grey[300],
             ),
           ],
         ),
